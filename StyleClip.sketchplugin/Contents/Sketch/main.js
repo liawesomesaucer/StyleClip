a07bc552d6ba2086e763e7f528d16ed996929edd
liawesomesaucer <vincent.oddrepublic@gmail.com>
2017-12-23 19:26:53 -0800

    MVP Functionality works for all 3 functions

--- StyleClip.sketchplugin/Contents/Sketch/main.js
+++ StyleClip.sketchplugin/Contents/Sketch/main.js
@@ -17,8 +17,8 @@ var clipRawCSS = function(context) {
   } else if (selection.count() > 1) {
     doc.showMessage("StyleClip: Please select a single element, not a group");
   } else {
-    var cssAttributes = '' + selection[0].CSSAttributeString();
-    doc.showMessage("StyleClip: Styles copied to clipboard!");
+    var cssAttributes = '' + selection[0].CSSAttributes().slice(1).join('\n');
+    doc.showMessage("StyleClip: CSS Styles copied to clipboard!");
     [pboard setString:cssAttributes forType:NSPasteboardTypeString];
   }
 }
@@ -35,23 +35,29 @@ var createSCSSGlobals = function(context) {
 
   // Find all existing global and document colors
   var documentColors = context.document.documentData().assets().colors();
-  // var globalColors = NSApp.delegate().globalAssets().colors();
 
   // Run color generation from ntc to get semantic color names
   var rgba, r, g, b, hex, colorName, line, color;
   var toClip = [];
-  for (var i = 0; i < documentColors.length; i++) {
+  for (var i = 0; i < documentColors.length; i++) { // TODO: extract this to a function
     rgba = documentColors[i].toString().replace('(', '').replace(')', '').split(' ');
     r = rgba[0].split(':')[1];
     g = rgba[1].split(':')[1];
     b = rgba[2].split(':')[1];
+    a = rgba[3].split(':')[1];
 
     hex = convertRgbToHex(r, g, b);
     colorName = ntc.name(hex)[1].toLowerCase().replace('/', '').replace(' ', '-');
-    // var line = colorName + ': rgb(' + r + ',' + g + ',' + b + ');';
-    line = colorName + ': ' + hex + ';';
+
+    if (a < 1) { // Handle non-opaque colors
+      colorName = colorName + '-' + a.slice(2, 4);
+      color = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+    } else {
+      color = hex;
+    }
+    line = '$' + colorName + ': ' + color + ';';
     toClip.push(line);
-  });
+  }
 
   // TODO: create dialog box for SASS
 
@@ -65,14 +71,48 @@ var createSCSSGlobals = function(context) {
 
 /**
  * Creates SCSS
+ *
+ * To run: CMD+Shift+I
  */
 var clipSCSS = function(context) {
   var doc = context.document;
   var selection = context.selection;
+
+  if(selection.count() == 0) {
+    doc.showMessage("StyleClip: Please select an element");
+    return;
+  } else if (selection.count() > 1) {
+    doc.showMessage("StyleClip: Please select a single element, not a group");
+    return;
+  }
+
   var pboard = NSPasteboard.generalPasteboard();
   [pboard clearContents];
 
-  // Get styles
+  // Find all used document colors
+  var documentColors = context.document.documentData().assets().colors();
+  var colorsMap = createRgbaToNameMap(documentColors);
+  var hexToNamesMap = createHexToNameMap(documentColors);
 
-  // Check if styles incorporate new global/document colors
+  // Get styles
+  // The slice is used to remove the default comment that comes with the rectangle
+  var rawCssAttributes = selection[0].CSSAttributes().slice(1);
+
+  // Replace variables
+  var toClip = '';
+  var color;
+  for (var i = 0; i < rawCssAttributes.length; i++) {
+    color = rawCssAttributes[i].split(': ')[1].slice(0, -1).toLowerCase();
+    log(color);
+    if (colorsMap[color]) {
+      toClip = toClip + rawCssAttributes[i].split(': ')[0] + ': $' + colorsMap[color] + ';';
+    } else if (hexToNamesMap[color]) {
+      toClip = toClip + rawCssAttributes[i].split(': ')[0] + ': $' + hexToNamesMap[color] + ';';
+    } else {
+      toClip = toClip + rawCssAttributes[i];
+    }
+  }
+  doc.showMessage("StyleClip: SCSS styles copied to clipboard!");
+
+  [pboard setString:toClip forType:NSPasteboardTypeString];
 }
--- StyleClip.sketchplugin/Contents/Sketch/rgbtohex.js
+++ StyleClip.sketchplugin/Contents/Sketch/rgbtohex.js
@@ -1,11 +1,62 @@
+/**
+ * Converts rgb decinal to hex code
+ */
 var rgbDecimalToHex = function (rgbDecimal) { 
-  var hex = Number(Math.round(rgbDecimal * 100)).toString(16);
+  var hex = Number(Math.floor(rgbDecimal * 256)).toString(16);
   if (hex.length < 2) {
-       hex = "0" + hex;
+    hex = "0" + hex;
+  }
+  if (hex.length > 2) {
+    hex = 'FF';
   }
   return hex;
 };
 
 var convertRgbToHex = function(r, g, b) {
   return '#' + [r, g, b].map(function(el) { return rgbDecimalToHex(el) }).join('');
+}
+
+/**
+ * Converts an array of document colors to a mapping from rgba string to colorName
+ */
+var createRgbaToNameMap = function(documentColors) {
+  var rgba, r, g, b, hex, colorName, color;
+  var colorsMap = {};
+  for (var i = 0; i < documentColors.length; i++) {
+    color = documentColors[i].toString();
+    rgba = color.slice(1, -1).split(' ');
+    r = rgba[0].split(':')[1];
+    g = rgba[1].split(':')[1];
+    b = rgba[2].split(':')[1];
+    a = rgba[3].split(':')[1];
+
+    hex = convertRgbToHex(r, g, b);
+    colorName = ntc.name(hex)[1].toLowerCase().replace('/', '').replace(' ', '-');
+
+    colorsMap[color] = colorName;
+  }
+  return colorsMap;
+}
+
+/**
+ * Converts an array of document colors to mapping from hex string to colorName
+ */
+var createHexToNameMap = function(documentColors) {
+  var rgba, r, g, b, hex, colorName, color;
+  var colorsMap = {};
+  for (var i = 0; i < documentColors.length; i++) {
+    color = documentColors[i].toString();
+    rgba = color.slice(1, -1).split(' ');
+    r = rgba[0].split(':')[1];
+    g = rgba[1].split(':')[1];
+    b = rgba[2].split(':')[1];
+    a = rgba[3].split(':')[1];
+
+    hex = convertRgbToHex(r, g, b);
+    colorName = ntc.name(hex)[1].toLowerCase().replace('/', '').replace(' ', '-');
+
+    colorsMap[hex] = colorName;
+    log(hex);
+  }
+  return colorsMap;
 }
